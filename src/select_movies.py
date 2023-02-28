2bcc5fdec12e046f49dc7e5a22d26926a0b27718
infoomics <sbx_94@hotmail.com>
2023-02-28 15:32:26 +0000

    reorganise building of query_str for location parameter

--- src/select_movies.py
+++ src/select_movies.py
@@ -2,7 +2,7 @@ from config.connection import con
 from pg8000.native import literal
 
 
-def select_movies(order_by='title', order='ASC', min_rating=-1):
+def select_movies(order_by='title', order='ASC', min_rating=-1, location=None):
     if order_by not in ['title', 'cost', 'rating', 'release_date']:
         raise Exception('Invalid order_by column')
 
@@ -11,7 +11,18 @@ def select_movies(order_by='title', order='ASC', min_rating=-1):
 
     min_rating = literal(min_rating)
 
-    query_str = f'SELECT * FROM movies WHERE COALESCE(rating,-1) >= {min_rating} ORDER BY {order_by} {order};'
+    query_str = f'SELECT * FROM movies'
+
+    if location != None:
+        query_str += ' JOIN stock USING (movie_id) JOIN stores USING (store_id)'
+
+    query_str += f' WHERE COALESCE(rating,-1) >= {min_rating}'
+
+    if location != None:
+        query_str += f' AND city = {literal(location)}'
+
+    query_str += f' ORDER BY {order_by} {order};'
+
     rows = con.run(query_str)
     columns = [meta['name'] for meta in con.columns]
     movies = [{column: row[i]
