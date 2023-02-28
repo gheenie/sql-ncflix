8dd0831c17a9630ec628ee26409ad4b8ff80e377
infoomics <sbx_94@hotmail.com>
2023-02-28 16:59:29 +0000

    expand select_rentals() for task 2.6.2

--- src/select_movies.py
+++ src/select_movies.py
@@ -30,3 +30,49 @@ def select_movies(order_by='title', order='ASC', min_rating=-1, location=None):
               for row in rows]
     
     return movies
+
+
+def select_rentals():
+    query_str = '''SELECT store_id, city, COUNT(customer_id) as number_of_customers
+        FROM rentals
+        JOIN stock USING (stock_id)
+        JOIN stores USING (store_id)
+        JOIN customers USING (customer_id)
+        WHERE location = city
+        GROUP BY store_id, city;'''
+
+    rows = con.run(query_str)
+    columns = [meta['name'] for meta in con.columns]
+    rentals = [{column: row[i]
+              for (i, column) in enumerate(columns)}
+              for row in rows]
+
+    print(rentals)
+
+    query_str = '''SELECT store_id, city, COUNT(DISTINCT(movie_id)) as number_of_films FROM stores JOIN stock USING (store_id) GROUP BY store_id, city;'''
+
+    rows = con.run(query_str)
+    columns = [meta['name'] for meta in con.columns]
+    rentals = [{column: row[i]
+              for (i, column) in enumerate(columns)}
+              for row in rows]
+
+    print(rentals)
+
+    query_str = '''SELECT store_id, city, COUNT(customer_id) as number_of_customers, COUNT(DISTINCT(movie_id)) as number_of_films 
+        FROM rentals
+        RIGHT JOIN stock USING (stock_id)
+        RIGHT JOIN stores USING (store_id)
+        RIGHT JOIN customers USING (customer_id)
+        WHERE location = city
+        GROUP BY store_id, city;'''
+
+    rows = con.run(query_str)
+    columns = [meta['name'] for meta in con.columns]
+    rentals = [{column: row[i]
+              for (i, column) in enumerate(columns)}
+              for row in rows]
+
+    print(rentals)
+
+    return rentals
