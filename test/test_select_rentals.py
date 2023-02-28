1db3fdacbabe11400b352e44e36ee86da010c71a
infoomics <sbx_94@hotmail.com>
2023-02-28 18:19:20 +0000

    add test for number_of_films; refactor tests to reflect new structure of select_rentals()

--- test/test_select_rentals.py
+++ test/test_select_rentals.py
@@ -1,5 +1,5 @@
 import pytest
-from src.select_movies import (select_rentals)
+from src.select_rentals import (select_rentals)
 from config.connection import con
 
 
@@ -7,17 +7,33 @@ def teardown_module():
     con.close()
 
 
-def test_select_rentals_returns_correct_columns():
-    expected = ['store_id', 'city', 'number_of_customers']
-
-    first_rental = select_rentals()[0]
-
-    assert list(first_rental.keys()) == expected
-
-
-def test_select_rentals_returns_correct_number_of_rows():
+def test_number_of_customers_property_is_correct_length():
     expected = 2
 
-    rentals = select_rentals()
+    cities_with_number_of_customers = select_rentals()['cities_with_number_of_customers']
 
-    assert len(rentals) == expected
+    assert len(cities_with_number_of_customers) == expected
+
+
+def test_number_of_customers_property_has_correct_columns():
+    expected = ['store_id', 'city', 'number_of_customers']
+
+    first_city_with_number_of_customers = select_rentals()['cities_with_number_of_customers'][0]
+
+    assert list(first_city_with_number_of_customers.keys()) == expected
+
+
+def test_number_of_films_property_is_correct_length():
+    expected = 4
+
+    cities_with_number_of_films = select_rentals()['cities_with_number_of_films']
+
+    assert len(cities_with_number_of_films) == expected
+
+
+def test_number_of_films_property_has_correct_columns():
+    expected = ['store_id', 'city', 'number_of_films']
+
+    first_city_with_number_of_films = select_rentals()['cities_with_number_of_films'][0]
+
+    assert list(first_city_with_number_of_films.keys()) == expected
